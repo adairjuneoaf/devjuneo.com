a3c940f19ad31279357e50b71d15aeb59849ebdb
Adair Juneo <adair_juneo@hotmail.com>
2024-08-22 19:01:13 -0300

    setup devjuneo.com

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <React.Fragment>
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="text-2xl">devjuneo.com</h1>
+      </div>
+    </React.Fragment>
+  );
+};
